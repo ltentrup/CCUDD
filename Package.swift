5eebabb9757b3b5ab0c5f25fee489525a9553a7a
Leander Tentrup <tentrup@react.uni-saarland.de>
2019-04-09 11:03:54 +0200

    Update to PackageDescription v4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "CCUDD",
+    products: [
+        .library(name: "CCUDD", targets: ["CCUDD"]),
+    ],
     targets: [
-        Target(name: "CCUDD"),
+        .target(name: "CCUDD"),
     ]
 )
